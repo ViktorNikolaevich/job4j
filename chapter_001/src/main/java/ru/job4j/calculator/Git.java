da514f87e59b3d659bf7a954ab5752ca40312a43
Vikotr Kotovskiy <redmi29v@gmail.com>
2019-09-12 08:33:24 +0300

    Интеграция Git и IDEA [#159462]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Git.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: operator
+ * Date: 12.09.19
+ * Time: 8:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class Git {
+}
